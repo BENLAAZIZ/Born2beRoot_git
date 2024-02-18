80ecde619ec1e55d73d82f771d950a183b627f71
BEN LAAZIZ HAMZA <99501397+BENLAAZIZ@users.noreply.github.com>
2024-02-18 16:52:48 +0100

    Update minitoring.sh

--- minitoring.sh
+++ minitoring.sh
@@ -8,13 +8,13 @@ numbervcpu=$(lscpu | grep "^CPU(s)" | awk '{print $2}')
 #numbervcpu=$(nproc)
 
 
-memory_usage=$(free -m | grep "Mem" | awk '{printf("%d/%dMB (%.2f%%) \n", $3, $2, ($3 * 100 / $2))}')
+memory_usage=$(free --mega | grep "Mem" | awk '{printf("%d/%dMB (%.2f%%) \n", $3, $2, ($3 * 100 / $2))}')
 
 fdisk=$(df -BG | grep "^/dev" | grep -v "/boot" | awk '{f += $2} END {print f}')
 
 udisk=$(df -BM | grep "^/dev" | grep -v "/boot" | awk '{u += $3} END {print u}')
 
-pdisk=$(df -BM | grep "^/dev" | grep -v "/boot" | awk '{f += $2; u += $3} END {printf("%d", u*100/f)}')
+pdisk=$(df -BM | grep "^/dev" | grep -v "/boot" | awk '{f += $2; u += $3} END {printf("%.2f%%", (u * 100) / f)}')
 
 cpu_load=$(mpstat | grep all | awk '{print 100 - $13}')
 
@@ -26,7 +26,7 @@ coni_tcp=$(netstat -an | grep "ESTABLISHED" | wc -l)
 
 user_log=$(who | cut -d " " -f 1 | sort -u | wc -l)
 
-network=$(echo "IP $(hostname -I) $(ip link show | grep "ether" | awk '{printf("(%s)", $2)}')")
+network=$(echo "IP $(hostname -I) $(ip link show | grep "ether" | awk '{print $2)')")
 
 nSudo=$(echo "$(journalctl -q _COMM=sudo | grep COMMAND | wc -l) cmd")
 
